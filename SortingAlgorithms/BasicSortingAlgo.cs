c5976bb464a5914f6abb7199355f3cdadfa11297
soumyata1997@gmail.com <soumyata1997@gmail.com>
2025-03-26 20:47:45 -0400

    Added new class library for C# sorting algorithms

---
+++ SortingAlgorithms/BasicSortingAlgo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    public class BasicSortingAlgo
+    {
+        public static void SelectionSortFunc(int[] arr)
+        {
+            for(int i=0;i<arr.Length-1;i++)
+            {
+                int min = i;
+                for (int j=i+1;j<arr.Length;j++)
+                {                    
+                    if (arr[j] < arr[min])
+                        min = j;
+                }
+                int temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+
+            Console.WriteLine("Sorted Array using Selection Sort: ");
+            for(int i =0;i<arr.Length;i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        public static void BubbleSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[min])
+                        min = j;
+                }
+                int temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+
+            Console.WriteLine("Sorted Array using Selection Sort: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
